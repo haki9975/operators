d4798026649e783616a280945682c79b159ad7ba
haki9975 <elias.robert.hakim@gmail.com>
2021-02-02 20:50:31 -0500

    commit tree 62d9b3b27d82af8aa64b61a96d950ef8f96434d9

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,22 @@
 def unsafe?(speed)
+    if speed > 60 
+       return true
+    end
 
+    if speed < 40
+        return true
+    else 
+        return false
+    end
 end
 
-
-
 def not_safe?(speed)
-	
+    if speed > 60
+        return true
+    end
+
+    speed = speed < 40 ? true: false
+
 end
 	
 
